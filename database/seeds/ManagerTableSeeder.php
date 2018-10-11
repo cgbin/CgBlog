176792e6e61de4b9f2f7c8d3644dd852efb658a7
cgbin <35454704+cgbin@users.noreply.github.com>
2018-10-11 23:06:00 +0800

    v4

--- database/seeds/ManagerTableSeeder.php
+++ database/seeds/ManagerTableSeeder.php
@@ -21,7 +21,7 @@ class ManagerTableSeeder extends Seeder
                 'role_id' => rand(1,5),
                 'created_at' => date('Y-m-d H:i:s',time()),
                 'updated_at' => date('Y-m-d H:i:s',time()),
-                'status' => rand(1)
+                'status' => '1'
 
             ];
         for ($i=1; $i <10 ; $i++) {
