24145346773098cc9628bae455c33a4725795b00
cgbin <35454704+cgbin@users.noreply.github.com>
2018-10-11 23:04:04 +0800

    v4

--- database/seeds/ManagerTableSeeder.php
+++ database/seeds/ManagerTableSeeder.php
@@ -11,7 +11,20 @@ class ManagerTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=0; $i <10 ; $i++) {
+        $data = [];
+        $data[0] = [
+                'username' => 'admin',
+                'password' => bcrypt('123'),
+                'gender' => rand(1,3),
+                'mobile' => rand(10000000000,19999999999),
+                'email' => str_random(6).'@qq.com',
+                'role_id' => rand(1,5),
+                'created_at' => date('Y-m-d H:i:s',time()),
+                'updated_at' => date('Y-m-d H:i:s',time()),
+                'status' => rand(1)
+
+            ];
+        for ($i=1; $i <10 ; $i++) {
             $data[] = [
                 'username' => str_random(6),
                 'password' => bcrypt('123'),
