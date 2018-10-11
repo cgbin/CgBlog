82cbc79d8ff4a9753a2839663a4bb2ef92333b00
cgbin <35454704+cgbin@users.noreply.github.com>
2018-10-11 20:08:59 +0800

    v2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(ManagerTableSeeder::class);
-        //$this->call(RoleTableSeeder::class);
-        //$this->call(AuthTableSeeder::class);
-        //$this->call(MemberTableSeeder::class);
-        //$this->call(ArticleTableSeeder::class);
-        //$this->call(CategoryTableSeeder::class);
-        //$this->call(LinksTableSeeder::class);
-        //$this->call(NavsTableSeeder::class);
-        //$this->call(ConfigsTableSeeder::class);
+        $this->call(ManagerTableSeeder::class);
+        $this->call(RoleTableSeeder::class);
+        $this->call(AuthTableSeeder::class);
+        $this->call(MemberTableSeeder::class);
+        $this->call(ArticleTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
+        $this->call(LinksTableSeeder::class);
+        $this->call(NavsTableSeeder::class);
+        $this->call(ConfigsTableSeeder::class);
     }
 }
